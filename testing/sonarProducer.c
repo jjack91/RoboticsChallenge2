afe96a3a75855c2a9aaf1614244d416992df9dbe
Tenma Rollins <tenmarollins@gmail.com>
2018-04-19 09:55:10 -0700

    added some documentation to sensor thread

--- testing/sonarProducer.c
+++ testing/sonarProducer.c
@@ -1,15 +1,13 @@
-#include <math.h>
-
-/* Constants */
+/* Private Constants */
 static const int MAX_DISTANCE = 90; // in cm 
 static const int SONAR_BUFFER_SIZE = 50;
 static const int AVG_CHECK_SIZE = 10;
 
-/* */
+/* Private Function Prototypes */
 static void incrementSonarIndex(int *index);
 static int getNextDistance();
 
-/* */
+/* Public Function Prototypes */
 int isObjectFound();
 int getDistance();
 
@@ -21,6 +19,9 @@ static int objectFound = 0;
 static int currentDistance = 0;
 
 
+/* Tasks to populate the distances retrieved from the sonar sensor
+   into a buffer that tells the robot how close they are to an object
+   if they are within about 3 feet of it. */
 task populateSonarValues() {
     
   while(true) {        
@@ -30,7 +31,7 @@ task populateSonarValues() {
       objectFound = 1;
       // ensure currentDistance is less than previous distance where possible
       if (sonarProducerIndex > 0 {
-        while (currentDistance-- < sonarDistanceBuffer[sonarProducerIndex - 1]) {}
+        while (currentDistance-- > sonarDistanceBuffer[sonarProducerIndex - 1]) {}
       }
       // load up the buffer
       sonarDistanceBuffer[sonarProducerIndex] = currentDistance;
@@ -53,10 +54,14 @@ task populateSonarValues() {
 
 }
 
+/* Increments the given index by 1. The given index will not
+	 exceed BUFFER_SIZE. */
 static void incrementIndex(int *index) {
 	*index = ++*index % BUFFER_SIZE;
 }
 
+/* Gets the next normalized distance measurement from the
+   sonar sensor. */
 staic int getNextDistance() {
   int distance = 0;
   for (int i = 0; i < AVG_CHECK_SIZE; i++) {
@@ -66,10 +71,15 @@ staic int getNextDistance() {
 }
 
 
+/* [Possibly Deprecated] */
+/* Checks whether or not an object is found.
+   Returns 1 if found, 0 otherwise. */ 
 int isObjectFound() {
   return objectFound;
 }
 
+/* Provides the next normalized distance from the 
+   distance buffer and increments the proper pointers */
 int getDistance() {
   if (objectFound == 1) {
     int distance = sonarDistanceBuffer[sonarConsumerIndex];

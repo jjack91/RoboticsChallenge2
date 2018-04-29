ec0f9d49e28ee962aa26355d246cfa6588ba09b3
Tenma Rollins <tenmar@uw.edu>
2018-04-29 13:37:49 -0700

    simplified sonar producer thread
    should be particularly simple now

--- sonarProducer.c
+++ sonarProducer.c
@@ -1,20 +1,18 @@
+#define SONAR_SENSOR_VAL S1
 /* Private Constants */
-static const int MAX_DISTANCE = 90; // in cm 
-static const int SONAR_BUFFER_SIZE = 50;
-static const int AVG_CHECK_SIZE = 10;
+static const int MAX_DISTANCE = 90; // in cm
+static const int MIN_DISTANCE = 5; // in cm
+static const int AVG_CHECK_SIZE = 15;
 
 /* Private Function Prototypes */
-static void incrementSonarIndex(int *index);
-static int getNextDistance();
+static int sonar_getNextDistance();
 
 /* Public Function Prototypes */
-int isObjectFound();
-int getDistance();
+int sonar_isObjectFound();
+int sonar_getDistance();
+float sonar_getProportion();
 
 /* Private Variables */
-static int sonarDistanceBuffer[SONAR_BUFFER_SIZE];
-static int sonarProducerIndex = 0;
-static int sonarConsumerIndex = 0;
 static int objectFound = 0;
 static int currentDistance = 0;
 
@@ -22,51 +20,35 @@ static int currentDistance = 0;
 /* Tasks to populate the distances retrieved from the sonar sensor
    into a buffer that tells the robot how close they are to an object
    if they are within about 3 feet of it. */
-task populateSonarValues() {
-    
-  while(true) {        
-    currentDistance = getNextDistance(); // get noise-filtered distance
+task populateSonarValues() 
+{
+  while(true) 
+	{
+		
+		displayBigTextLine(4, "SONAR SENSOR:");
+    currentDistance = sonar_getNextDistance(); // get noise-filtered distance
+		displayBigTextLine(6, "DISTANCE: %d", currentDistance);
     // when object detected
-    if (currentDistance <= MAX_DISTANCE) {
+    if (currentDistance <= MAX_DISTANCE && currentDistance >= MIN_DISTANCE) 
+    {
       objectFound = 1;
-      // ensure currentDistance is less than previous distance where possible
-      if (sonarProducerIndex > 0 {
-        while (currentDistance-- > sonarDistanceBuffer[sonarProducerIndex - 1]) {}
-      }
-      // load up the buffer
-      sonarDistanceBuffer[sonarProducerIndex] = currentDistance;
-      incrementSonarIndex(&sonarProducerIndex);
-    }
-    
-    // no object detected
-    if (currentDistance > MAX_DISTANCE) {
+			displayBigTextLine(8, "OBJECT: FOUND");
+    } 
+    else 
+    {
       objectFound = 0;
-      // purge buffer, no object detected 
-      sonarDistanceBuffer[0] = -1
-      sonarProducerIndex = 0;
-      sonarConsumerIndex = 0;
-    }
-    
-    // dont try to populate buffer if we havent consumed 
-    if (sonarProducerIndex == sonarConsumerIndex) {
-      abortTimeSlice();
+			displayBigTextLine(8, "OBJECT: NOT FOUND");
     }
   }
 
 }
 
-/* Increments the given index by 1. The given index will not
-	 exceed BUFFER_SIZE. */
-static void incrementIndex(int *index) {
-	*index = ++*index % BUFFER_SIZE;
-}
-
 /* Gets the next normalized distance measurement from the
    sonar sensor. */
-staic int getNextDistance() {
+static int sonar_getNextDistance() {
   int distance = 0;
   for (int i = 0; i < AVG_CHECK_SIZE; i++) {
-    distance += SensorValue[sonar1];
+    distance += SensorValue[SONAR_SENSOR_VAL];
   }
   return distance / AVG_CHECK_SIZE;
 }
@@ -74,26 +56,24 @@ staic int getNextDistance() {
 
 /* [Possibly Deprecated] */
 /* Checks whether or not an object is found.
-   Returns 1 if found, 0 otherwise. */ 
-int isObjectFound() {
+   Returns 1 if found, 0 otherwise. */
+int sonar_isObjectFound() {
   return objectFound;
 }
 
-/* Provides the next normalized distance from the 
+/* Provides the next normalized distance from the
    distance buffer and increments the proper pointers */
-int getDistance() {
-  if (objectFound == 1) {
-    int distance = sonarDistanceBuffer[sonarConsumerIndex];
-    incrementSonarIndex(&sonarConsumerIndex);
-    return distance;
-  } else {
+int sonar_getDistance() {
+  if (objectFound == 1) 
+  {
+    return currentDistance;
+  } 
+  else 
+  {
     return -1;
   }
 }
 
-
-
-
-
-
-
+float sonar_getProportion() {
+	return currentDistance * 1.0 / MAX_DISTANCE;
+}
